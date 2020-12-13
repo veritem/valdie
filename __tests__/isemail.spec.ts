14a29838aacfb2c5aa20e2fef67ca69316de41ed
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-12-13 05:46:33 +0200

    fixed error with error exception testing

--- __tests__/isemail.spec.ts
+++ __tests__/isemail.spec.ts
@@ -6,7 +6,6 @@ test('check for email validity', () => {
   expect(isEmail('email@example.com')).toBeTruthy()
 })
 
-//TODO: test a function that return an error exception
 test('check if email validator fails', () => {
-  expect(() => isEmail('”more unusual”@example.com')).toThrow(TypeError)
+  expect(() => isEmail('mmmm')).toThrow()
 })
