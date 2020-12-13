030cf33ed7c97a3123725bd754e21176da41e8b4
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-12-13 05:56:53 +0200

    grouped all tests case for isEmail

--- __tests__/isemail.spec.ts
+++ __tests__/isemail.spec.ts
@@ -1,11 +1,15 @@
 import { isEmail } from '../src/index'
 
-test('check for email validity', () => {
-  expect(isEmail('email@example.com')).toBeTruthy()
-  expect(isEmail('_______@example.com')).toBeTruthy()
-  expect(isEmail('email@example.com')).toBeTruthy()
-})
+describe('isEMail', () => {
+  it('check for email validity', () => {
+    expect(isEmail('email@example.com')).toBeTruthy()
+    expect(isEmail('_______@example.com')).toBeTruthy()
+    expect(isEmail('email@example.com')).toBeTruthy()
+  })
 
-test('check if email validator fails', () => {
-  expect(() => isEmail('mmmm')).toThrow(new Error('mmmm is not a valid email'))
+  it('check if email validator fails', () => {
+    expect(() => isEmail('mmmm')).toThrow(
+      new Error('mmmm is not a valid email')
+    )
+  })
 })
