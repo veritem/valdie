bab621e192afe636d9cf49e78e6035bf6f4ac61b
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-12-13 05:30:47 +0200

    added unit tests for an email

--- __tests__/isemail.spec.ts
+++ __tests__/isemail.spec.ts
@@ -5,3 +5,8 @@ test('check for email validity', () => {
   expect(isEmail('_______@example.com')).toBeTruthy()
   expect(isEmail('email@example.com')).toBeTruthy()
 })
+
+//TODO: test a function that return an error exception
+test('check if email validator fails', () => {
+  expect(() => isEmail('”more unusual”@example.com')).toThrow(TypeError)
+})
