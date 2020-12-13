9d6f33f2e0a7ee9056ef5b4b794a41e3175ebbe9
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-12-13 05:13:18 +0200

    added more tests cases for isEmail

--- __tests__/isemail.spec.ts
+++ __tests__/isemail.spec.ts
@@ -2,4 +2,6 @@ import { isEmail } from '../src/index'
 
 test('check for email validity', () => {
   expect(isEmail('email@example.com')).toBeTruthy()
+  expect(isEmail('_______@example.com')).toBeTruthy()
+  expect(isEmail('email@example.com')).toBeTruthy()
 })
