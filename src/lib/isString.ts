5546dbbf5282a5ab1a582d542482f8b7dc5328d5
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-01-23 19:21:18 +0200

    added tests for isString utility function

--- src/lib/isString.ts
+++ src/lib/isString.ts
@@ -1,7 +1,7 @@
 import ErrorMessage from './utils/Error'
 
 //TODO:  function to check if the passed string is a string
-export default function isString(str: unknown) {
+export default function isString(str: unknown):boolean {
   if (typeof str !== 'string') ErrorMessage(str, 'string')
   return true
 }
