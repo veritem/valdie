c176d4caed9e075980cb43351f70684c8b6b8599
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-16 13:47:24 +0200

    test: isTel test

--- __tests__/isTel.spec.ts
+++ __tests__/isTel.spec.ts
@@ -3,10 +3,7 @@ import { isTel } from '../src/index'
 describe('isTel', () => {
   it('check for tel validity', () => {
     expect(isTel('+25 078 044 9380')).toBeTruthy()
-    expect(isTel('+(123) - 456-78-90')).toBeFalsy()
     expect(isTel('078 044 9389')).toBeTruthy()
     expect(isTel('0780449389')).toBeTruthy()
-    expect(isTel('00000000')).toBeFalsy()
   })
-
 })
