cd710a1e7273bd6a84c9e1e652f993ca05c30bb2
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-25 14:18:46 +0200

    docs: is String

--- src/isString.ts
+++ src/isString.ts
@@ -1,6 +1,11 @@
 import ErrorMessage from './utils/Error'
 
 //TODO:  function to check if the passed string is a string
+/**
+ * Check if valus is a string
+ * @param value value to check
+ * @returns true if value is valid
+ */
 export function isString(value: unknown): boolean {
   if (typeof value !== 'string') ErrorMessage(value, 'string')
   return true
