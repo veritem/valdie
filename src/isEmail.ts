f413772b8e1f8c0388df6602b65c8a34447b0848
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-25 14:08:30 +0200

    docs: is email fn

--- src/isEmail.ts
+++ src/isEmail.ts
@@ -4,6 +4,11 @@ import ErrorMessage from './utils/Error'
 const emailRegex =
   /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
 
+/**
+ * Check if value is valid email address
+ * @param str value to check
+ * @returns true if value is valid
+ */
 export function isEmail(str: string): boolean {
   if (!emailRegex.test(str)) ErrorMessage(str, 'email')
   return true
