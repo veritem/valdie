568cc11c50581c29539f356de786feaa025f905b
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-25 14:11:17 +0200

    docs: is Integer

--- src/isInteger.ts
+++ src/isInteger.ts
@@ -1,6 +1,11 @@
 import ErrorMessage from './utils/Error'
 
 /* eslint-disable no-useless-escape */
+/**
+ * Check if value is valid integer
+ * @param value value to check 
+ * @returns true if value is valid
+ */
 export function isInteger(value: number): boolean {
   if (!Number.isInteger(value)) ErrorMessage(value, 'integer')
   return true
