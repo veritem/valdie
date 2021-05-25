a1ab052a4adbf05ae7bb399f8d837e42a16bc64a
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-25 14:18:57 +0200

    docs: is Tel

--- src/isTel.ts
+++ src/isTel.ts
@@ -4,6 +4,11 @@ import ErrorMessage from './utils/Error'
 const telRegex =
   /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/i
 
+/**
+ * Check if value is valid telephone number
+ * @param value value to check
+ * @returns true if value is valid
+ */
 export function isTel(value: string): boolean {
   if (!telRegex.test(value)) ErrorMessage(value, 'tel')
   return true
