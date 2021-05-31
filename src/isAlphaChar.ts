78db1c7bb514248420fc280fc67fe989c152a8d4
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-31 23:30:47 +0200

    feat: isAlphaChars

---
+++ src/isAlphaChar.ts
@@ -0,0 +1,15 @@
+import ErrorMessage from './utils/Error'
+
+/**
+ * Check if value is valid string with only specified character, without other characters or numbers
+ * @param value value to check
+ * @param separator any string separator
+ * @returns true if value is valid
+ */
+export function isAlphaChar(value: string, separator = '-'): boolean {
+  /* eslint-disable no-useless-escape */
+  const alphaRegex = new RegExp('^([a-zA-Z\\' + separator + ']+)$', 'i')
+
+  if (!alphaRegex.test(value)) ErrorMessage(value, `alphaChar ${separator} `)
+  return true
+}
