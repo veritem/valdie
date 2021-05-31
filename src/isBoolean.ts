7537d1ab0a3ec372183b8409eea18d3c5bf92345
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-06-01 00:17:27 +0200

    feat: isBoolean

---
+++ src/isBoolean.ts
@@ -0,0 +1,22 @@
+import ErrorMessage from './utils/Error'
+
+/**
+ * Check if value is valid array type
+ * @param value value to check
+ * @returns true if value is valid
+ */
+export function isBoolean(value: string | boolean | number): boolean {
+  const v = '' + value
+
+  if (
+    v === 'false' ||
+    v === 'true' ||
+    v === 'yes' ||
+    v === 'no' ||
+    v === '1' ||
+    v === '0'
+  )
+    return true
+  else ErrorMessage(value, 'array')
+  return false
+}
