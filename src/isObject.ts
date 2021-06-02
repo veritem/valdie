29b0a4efc5a86cf4264fd16d7993997ed2d6e2fe
yombi <ntwariegide2@gmail.com>
2021-06-02 15:23:26 +0200

    Fit: fix bug in isObject method

--- src/isObject.ts
+++ src/isObject.ts
@@ -1,9 +1,7 @@
 import ErrorMessage from './utils/Error'
 
 export default function isObject(value: any): boolean {
+  if (typeof value !== 'object' && value !== null) ErrorMessage(value, 'object')
 
-    if(typeof value !== 'object' && value !== null ) ErrorMessage(value,"object")
-
-    return true
-
+  return true
 }
