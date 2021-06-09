ee08538d6f2fe406bc47fe705ef8e4dcc4ab0576
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-06-09 16:16:32 +0200

    test: is Alpha

---
+++ __tests__/isAlpha.spec.ts
@@ -0,0 +1,21 @@
+import { isAlpha } from '../src/index'
+
+describe('isAlpha', () => {
+  it('check for alphabet validity', () => {
+    expect(isAlpha('email')).toBeTruthy()
+    expect(isAlpha('examplecom')).toBeTruthy()
+    expect(isAlpha('emailcom')).toBeTruthy()
+  })
+
+  it('check if alpha validator fails', () => {
+    expect(() => isAlpha('mmmm5487u#')).toThrow(
+      new Error('mmmm5487u# is not a valid alpha')
+    )
+  })
+
+  it('check if alpha validator fails', () => {
+    expect(() => isAlpha('hell@')).toThrow(
+      new Error('hell@ is not a valid alpha')
+    )
+  })
+})
