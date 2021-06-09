fdf0f832c1e1005c5eb2d4dd91f45913ffa3eaa9
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-06-09 17:29:22 +0200

    perf: check for substrings

--- src/lib/isAlphaChar.ts
+++ src/lib/isAlphaChar.ts
@@ -7,10 +7,10 @@ import ErrorMessage from '../utils/Error'
  * @returns true if value is valid
  */
 const alphaCharRegex = /(^[a-zA-Z!@#$%^&*(),.?":{}|<>_~`[\];]+)$/
-
+const alphaRegex = /'^([a-zA-Z]+)$'/
 export function isAlphaChar(value: string): boolean {
   /* eslint-disable no-useless-escape */
-
+  if (alphaRegex.test(value)) ErrorMessage(value, `alphaChar`) // make sure string is not alpha only
   if (!alphaCharRegex.test(value)) ErrorMessage(value, `alphaChar`)
   return true
 }
--- src/lib/isAlphaNum.ts
+++ src/lib/isAlphaNum.ts
@@ -2,6 +2,7 @@ import ErrorMessage from '../utils/Error'
 
 /* eslint-disable no-useless-escape */
 const alphaNumRegex = /^([a-zA-Z\d]+)$/i
+const alphaRegex = /'^([a-zA-Z]+)$'/
 
 /**
  * Check if value is valid string with only numbers without other characters
@@ -9,6 +10,7 @@ const alphaNumRegex = /^([a-zA-Z\d]+)$/i
  * @returns true if value is valid
  */
 export function isAlphaNum(value: string): boolean {
+  if (alphaRegex.test(value) || Number(value)) ErrorMessage(value, `alphaNum`) // make sure string is not alpha only or numbers only
   if (!alphaNumRegex.test(value)) ErrorMessage(value, 'alphaNum')
   return true
 }
