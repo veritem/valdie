8f250865e1bda1baa8327c82b55f84d51144e724
Divin Irakiza <irakizadivin@gmail.com>
2021-06-14 11:17:42 +0200

    new: isPassword tests done

--- __tests__/isPassword.spec.ts
+++ __tests__/isPassword.spec.ts
@@ -4,27 +4,28 @@ import { isPassword } from "../src/index";
 describe('isPassword', () => {
 
     it('Checks if password is TOO_WEAK', () => {
-        expect(() => isPassword('password', PASSWORDS.TOO_WEAK)).toThrow(new Error('password is not a valid TOO_WEAK password'))
-        expect(() => isPassword('password', PASSWORDS.TOO_WEAK)).toThrow(new Error('password is not a valid TOO_WEAK password'))
+        expect(() => isPassword('ab', PASSWORDS.TOO_WEAK)).toBeTruthy()
+        expect(() => isPassword('123', PASSWORDS.TOO_WEAK)).toThrow(new Error('123 is not a valid TOO_WEAK password'))
+
     })
 
     it('Checks if password is WEAK', () => {
-        expect(() => isPassword('password', PASSWORDS.WEAK)).toThrow(new Error('password is not a valid TOO_WEAK password'))
-        expect(() => isPassword('password', PASSWORDS.WEAK)).toThrow(new Error('password is not a valid TOO_WEAK password'))
+        expect(() => isPassword('passwo', PASSWORDS.WEAK)).toBeTruthy()
+        expect(() => isPassword('hello', PASSWORDS.WEAK)).toThrow(new Error('password is not a valid WEAK password'))
     })
     
     it('Checks if password is MEDIUM', () => {
-        expect(() => isPassword('password', PASSWORDS.MEDIUM)).toThrow(new Error('password is not a valid TOO_WEAK password'))
-        expect(() => isPassword('password', PASSWORDS.MEDIUM)).toThrow(new Error('password is not a valid TOO_WEAK password'))
+        expect(() => isPassword('12asswor3', PASSWORDS.TOO_WEAK)).toBeTruthy()
+        expect(() => isPassword('1passwor', PASSWORDS.MEDIUM)).toThrow(new Error('password is not a valid TOO_WEAK password'))
     })
     
     it('Checks if password is STRONG', () => {
-        expect(() => isPassword('password', PASSWORDS.STRONG)).toThrow(new Error('password is not a valid TOO_WEAK password'))
-        expect(() => isPassword('password', PASSWORDS.STRONG)).toThrow(new Error('password is not a valid TOO_WEAK password'))
+        expect(() => isPassword('@Paword1', PASSWORDS.TOO_WEAK)).toBeTruthy()
+        expect(() => isPassword('@paword4', PASSWORDS.STRONG)).toThrow(new Error('password is not a valid TOO_WEAK password'))
     })
 
     it('Checks if password is VERY_STRONG', () => {
-        expect(() => isPassword('password', PASSWORDS.VERY_STRONG)).toThrow(new Error('password is not a valid TOO_WEAK password'))
-        expect(() => isPassword('password', PASSWORDS.VERY_STRONG)).toThrow(new Error('password is not a valid TOO_WEAK password'))
+        expect(() => isPassword('#Hidr4dra10$', PASSWORDS.TOO_WEAK)).toBeTruthy()
+        expect(() => isPassword('password@123', PASSWORDS.VERY_STRONG)).toThrow(new Error('password is not a valid TOO_WEAK password'))
     })
 })
