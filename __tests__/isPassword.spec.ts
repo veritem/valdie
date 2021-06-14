86db752a3a6d753ef7498790e6e1cae41992ec1f
Divin Irakiza <irakizadivin@gmail.com>
2021-06-14 11:18:44 +0200

    new: isPassword tests done

--- __tests__/isPassword.spec.ts
+++ __tests__/isPassword.spec.ts
@@ -11,21 +11,21 @@ describe('isPassword', () => {
 
     it('Checks if password is WEAK', () => {
         expect(() => isPassword('passwo', PASSWORDS.WEAK)).toBeTruthy()
-        expect(() => isPassword('hello', PASSWORDS.WEAK)).toThrow(new Error('password is not a valid WEAK password'))
+        expect(() => isPassword('hello', PASSWORDS.WEAK)).toThrow(new Error('hello is not a valid WEAK password'))
     })
     
     it('Checks if password is MEDIUM', () => {
-        expect(() => isPassword('12asswor3', PASSWORDS.TOO_WEAK)).toBeTruthy()
-        expect(() => isPassword('1passwor', PASSWORDS.MEDIUM)).toThrow(new Error('password is not a valid TOO_WEAK password'))
+        expect(() => isPassword('12asswor3', PASSWORDS.MEDIUM)).toBeTruthy()
+        expect(() => isPassword('1passwor', PASSWORDS.MEDIUM)).toThrow(new Error('1passwor is not a valid MEDIUM password'))
     })
     
     it('Checks if password is STRONG', () => {
-        expect(() => isPassword('@Paword1', PASSWORDS.TOO_WEAK)).toBeTruthy()
-        expect(() => isPassword('@paword4', PASSWORDS.STRONG)).toThrow(new Error('password is not a valid TOO_WEAK password'))
+        expect(() => isPassword('@Paword1', PASSWORDS.STRONG)).toBeTruthy()
+        expect(() => isPassword('@paword4', PASSWORDS.STRONG)).toThrow(new Error('@paword4 is not a valid STRONG password'))
     })
 
     it('Checks if password is VERY_STRONG', () => {
-        expect(() => isPassword('#Hidr4dra10$', PASSWORDS.TOO_WEAK)).toBeTruthy()
-        expect(() => isPassword('password@123', PASSWORDS.VERY_STRONG)).toThrow(new Error('password is not a valid TOO_WEAK password'))
+        expect(() => isPassword('#Hidr4dra10$', PASSWORDS.VERY_STRONG)).toBeTruthy()
+        expect(() => isPassword('password@123', PASSWORDS.VERY_STRONG)).toThrow(new Error('password@123 is not a valid VERY_STRONG password'))
     })
 })
