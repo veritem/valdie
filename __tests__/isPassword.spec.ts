5b82deba11dce59ede4b71e39329da3d37954dcf
Divin Irakiza <irakizadivin@gmail.com>
2021-06-14 10:49:21 +0200

    new: isPassword tests initiation

--- __tests__/isPassword.spec.ts
+++ __tests__/isPassword.spec.ts
@@ -1,5 +1,30 @@
+import { PASSWORDS } from "../src/enums/EPasswords";
 import { isPassword } from "../src/index";
 
-// describe('isPassword', () => {
-//     it ('Checks for password validatidity')
-// })
+describe('isPassword', () => {
+
+    it('Checks if password is TOO_WEAK', () => {
+        expect(() => isPassword('password', PASSWORDS.TOO_WEAK)).toThrow(new Error('password is not a valid TOO_WEAK password'))
+        expect(() => isPassword('password', PASSWORDS.TOO_WEAK)).toThrow(new Error('password is not a valid TOO_WEAK password'))
+    })
+
+    it('Checks if password is WEAK', () => {
+        expect(() => isPassword('password', PASSWORDS.WEAK)).toThrow(new Error('password is not a valid TOO_WEAK password'))
+        expect(() => isPassword('password', PASSWORDS.WEAK)).toThrow(new Error('password is not a valid TOO_WEAK password'))
+    })
+    
+    it('Checks if password is MEDIUM', () => {
+        expect(() => isPassword('password', PASSWORDS.MEDIUM)).toThrow(new Error('password is not a valid TOO_WEAK password'))
+        expect(() => isPassword('password', PASSWORDS.MEDIUM)).toThrow(new Error('password is not a valid TOO_WEAK password'))
+    })
+    
+    it('Checks if password is STRONG', () => {
+        expect(() => isPassword('password', PASSWORDS.STRONG)).toThrow(new Error('password is not a valid TOO_WEAK password'))
+        expect(() => isPassword('password', PASSWORDS.STRONG)).toThrow(new Error('password is not a valid TOO_WEAK password'))
+    })
+
+    it('Checks if password is VERY_STRONG', () => {
+        expect(() => isPassword('password', PASSWORDS.VERY_STRONG)).toThrow(new Error('password is not a valid TOO_WEAK password'))
+        expect(() => isPassword('password', PASSWORDS.VERY_STRONG)).toThrow(new Error('password is not a valid TOO_WEAK password'))
+    })
+})
