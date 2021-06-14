0c9a3d5ec189ead1b07ccc06ee2c14a74c839be4
Divin Irakiza <irakizadivin@gmail.com>
2021-06-14 11:30:36 +0200

    isPassword Error arg modifications

--- __tests__/isPassword.spec.ts
+++ __tests__/isPassword.spec.ts
@@ -1,31 +1,39 @@
-import { PASSWORDS } from "../src/enums/EPasswords";
-import { isPassword } from "../src/index";
+import { PASSWORDS } from '../src/enums/EPasswords'
+import { isPassword } from '../src/index'
 
 describe('isPassword', () => {
+  it('Checks if password is TOO_WEAK', () => {
+    expect(() => isPassword('ab', PASSWORDS.TOO_WEAK)).toBeTruthy()
+    expect(() => isPassword('123', PASSWORDS.TOO_WEAK)).toThrow(
+      new Error('123 is not a valid TOO_WEAK password')
+    )
+  })
 
-    it('Checks if password is TOO_WEAK', () => {
-        expect(() => isPassword('ab', PASSWORDS.TOO_WEAK)).toBeTruthy()
-        expect(() => isPassword('123', PASSWORDS.TOO_WEAK)).toThrow(new Error('123 is not a valid TOO_WEAK password'))
+  it('Checks if password is WEAK', () => {
+    expect(() => isPassword('passwo', PASSWORDS.WEAK)).toBeTruthy()
+    expect(() => isPassword('hello', PASSWORDS.WEAK)).toThrow(
+      new Error('hello is not a valid WEAK password')
+    )
+  })
 
-    })
+  it('Checks if password is MEDIUM', () => {
+    expect(() => isPassword('12asswor3', PASSWORDS.MEDIUM)).toBeTruthy()
+    expect(() => isPassword('1passwor', PASSWORDS.MEDIUM)).toThrow(
+      new Error('1passwor is not a valid MEDIUM password')
+    )
+  })
 
-    it('Checks if password is WEAK', () => {
-        expect(() => isPassword('passwo', PASSWORDS.WEAK)).toBeTruthy()
-        expect(() => isPassword('hello', PASSWORDS.WEAK)).toThrow(new Error('hello is not a valid WEAK password'))
-    })
-    
-    it('Checks if password is MEDIUM', () => {
-        expect(() => isPassword('12asswor3', PASSWORDS.MEDIUM)).toBeTruthy()
-        expect(() => isPassword('1passwor', PASSWORDS.MEDIUM)).toThrow(new Error('1passwor is not a valid MEDIUM password'))
-    })
-    
-    it('Checks if password is STRONG', () => {
-        expect(() => isPassword('@Paword1', PASSWORDS.STRONG)).toBeTruthy()
-        expect(() => isPassword('@paword4', PASSWORDS.STRONG)).toThrow(new Error('@paword4 is not a valid STRONG password'))
-    })
+  it('Checks if password is STRONG', () => {
+    expect(() => isPassword('@Paword1', PASSWORDS.STRONG)).toBeTruthy()
+    expect(() => isPassword('@paword4', PASSWORDS.STRONG)).toThrow(
+      new Error('@paword4 is not a valid STRONG password')
+    )
+  })
 
-    it('Checks if password is VERY_STRONG', () => {
-        expect(() => isPassword('#Hidr4dra10$', PASSWORDS.VERY_STRONG)).toBeTruthy()
-        expect(() => isPassword('password@123', PASSWORDS.VERY_STRONG)).toThrow(new Error('password@123 is not a valid VERY_STRONG password'))
-    })
+  it('Checks if password is VERY_STRONG', () => {
+    expect(() => isPassword('#Hidr4dra10$', PASSWORDS.VERY_STRONG)).toBeTruthy()
+    expect(() => isPassword('password@123', PASSWORDS.VERY_STRONG)).toThrow(
+      new Error('password@123 is not a valid VERY_STRONG password')
+    )
+  })
 })
--- src/lib/isPassword.ts
+++ src/lib/isPassword.ts
@@ -1,46 +1,45 @@
-import ErrorMessage from '../utils/Error';
-import PasswordScore from '../utils/PasswordScore';
+import ErrorMessage from '../utils/Error'
+import PasswordScore from '../utils/PasswordScore'
 import { PASSWORDS, PASSWORD_SCORE } from '../enums/EPasswords'
-
 
 /**
  * Validates a given str to a given PASSWORD strength
  * @param str Password to check
  * @param type Password Strength Variation
- * @returns 
+ * @returns
  */
-export function isPassword(str: string, type: PASSWORDS=PASSWORDS.MEDIUM): boolean {
-
+export function isPassword(
+  str: string,
+  type: PASSWORDS = PASSWORDS.MEDIUM
+): boolean {
   switch (type) {
     case PASSWORDS.TOO_WEAK:
       if (PasswordScore(str) <= PASSWORD_SCORE[0])
-        ErrorMessage(str, `${type.toString()} password`)
-      break;
+        ErrorMessage(str, `${PASSWORDS[PASSWORDS.TOO_WEAK].toString()} password`)
+      break
 
     case PASSWORDS.WEAK:
       if (PasswordScore(str) <= PASSWORD_SCORE[1])
-        ErrorMessage(str, `${type.toString()} password`)
-      break;
-  
+        ErrorMessage(str, `${PASSWORDS[PASSWORDS.WEAK].toString()} password`)
+      break
+
     case PASSWORDS.MEDIUM:
       if (PasswordScore(str) <= PASSWORD_SCORE[2])
-        ErrorMessage(str, `${type.toString()} password`)
-      break;
+        ErrorMessage(str, `${PASSWORDS[PASSWORDS.MEDIUM].toString()} password`)
+      break
 
     case PASSWORDS.STRONG:
       if (PasswordScore(str) <= PASSWORD_SCORE[3])
-        ErrorMessage(str, `${type.toString()} password`)
-      break;
-    
-        
+        ErrorMessage(str, `${PASSWORDS[PASSWORDS.STRONG].toString()} password`)
+      break
+
     case PASSWORDS.VERY_STRONG:
       if (PasswordScore(str) <= PASSWORD_SCORE[4])
-        ErrorMessage(str, `${type.toString()} password`)
-        break;
-    
-  
+        ErrorMessage(str, `${PASSWORDS[PASSWORDS.VERY_STRONG].toString()} password`)
+      break
+
     default:
-      break;
+      break
   }
   return true
 }
