7cf8ae5b4d5f6b946b52fd45b6f7f71c44ff7c6e
Divin Irakiza <irakizadivin@gmail.com>
2021-06-14 10:39:15 +0200

    refactor!: extracted score fn()

--- src/lib/isPassword.ts
+++ src/lib/isPassword.ts
@@ -1,36 +1,6 @@
-import ErrorMessage from '../utils/Error'
+import ErrorMessage from '../utils/Error';
+import PasswordScore from '../utils/PasswordScore';
 import { PASSWORDS, PASSWORD_SCORE } from '../enums/EPasswords'
-
-const score = (str: string): number => {
-  let score = 0
-  if (!str) {
-    return score
-  }
-
-  const letters: any = new Object()
-  for (let i = 0; i < str.length; i++) {
-    letters[str[i]] = (letters[str[i]] || 0) + 1
-    score += 5.0 / letters[str[i]]
-  }
-
-  const variations: any = {
-    digits: /\d/.test(str),
-    lower: /[a-z]/.test(str),
-    upper: /[A-Z]/.test(str),
-    nonWords: /\W/.test(str)
-  }
-
-  let variationCount = 0
-
-  for (const check in variations) {
-    if (check !== undefined) {
-      variationCount += variations[check] === true ? 1 : 0
-    }
-  }
-  score += (variationCount - 1) * 10
-
-  return score
-}
 
 
 /**
@@ -43,28 +13,28 @@ export function isPassword(str: string, type: PASSWORDS=PASSWORDS.MEDIUM): boole
 
   switch (type) {
     case PASSWORDS.TOO_WEAK:
-      if (score(str) <= PASSWORD_SCORE[0])
+      if (PasswordScore(str) <= PASSWORD_SCORE[0])
         ErrorMessage(str, `${type.toString()} password`)
       break;
 
     case PASSWORDS.WEAK:
-      if (score(str) <= PASSWORD_SCORE[1])
+      if (PasswordScore(str) <= PASSWORD_SCORE[1])
         ErrorMessage(str, `${type.toString()} password`)
       break;
   
     case PASSWORDS.MEDIUM:
-      if (score(str) <= PASSWORD_SCORE[2])
+      if (PasswordScore(str) <= PASSWORD_SCORE[2])
         ErrorMessage(str, `${type.toString()} password`)
       break;
 
     case PASSWORDS.STRONG:
-      if (score(str) <= PASSWORD_SCORE[3])
+      if (PasswordScore(str) <= PASSWORD_SCORE[3])
         ErrorMessage(str, `${type.toString()} password`)
       break;
     
         
     case PASSWORDS.VERY_STRONG:
-      if (score(str) <= PASSWORD_SCORE[4])
+      if (PasswordScore(str) <= PASSWORD_SCORE[4])
         ErrorMessage(str, `${type.toString()} password`)
         break;
     
