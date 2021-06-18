2497b62fce153fcb65ea4cea126f045fd865febe
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-06-18 11:44:41 +0200

    feat: improve fn to use trimmed string

--- src/utils/methods.ts
+++ src/utils/methods.ts
@@ -9,7 +9,7 @@
  * @returns returns formated string
  */
 export function purifyStringObject(stringObject: string): string {
-  return stringObject.replace(/(\w+:)|(\w+ :)/g, function (s) {
+  return trim(stringObject, false).replace(/(\w+:)|(\w+ :)/g, function (s) {
     return '"' + s.substring(0, s.length - 1) + '":'
   })
 }
