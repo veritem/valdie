01a62f0c5f215ed4ab0f99f9a77a1660e9cfdd7b
pacifiquem <pacifiquemurangwa001@gmail.com>
2023-01-10 08:48:15 +0200

    feat: isDistinct added

---
+++ src/lib/isDistinct.ts
@@ -0,0 +1,32 @@
+import { ErrorMessage, Response, SuccessMesage } from '../utils/response'
+
+/* eslint-disable no-useless-escape */
+
+/**
+ * Check if array provided is distinct {no duplicates},
+ * except only array
+ * @param array array to check
+ * @returns true if array does not have duplicates
+ */
+
+export function isDistinct(array: unknown[]): Response {
+
+    try {
+
+        let setForArrayValues:Set<unknown> = new Set();
+
+        array.map(item => {
+            setForArrayValues.add(item);
+        });
+    
+        //set can't contain duplicates, so if array is distinct its length must be equal to the one of its set .
+        if(array.length === setForArrayValues.size) 
+            return SuccessMesage();
+    
+        return ErrorMessage(`${array} is not distinct}`);
+
+    } catch (error) {
+        return ErrorMessage(`${array} is not distinct}`);
+    }
+
+}
