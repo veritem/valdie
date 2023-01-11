6ac02359d08f787e0289c55cca116f2212f87e6b
Makuza Mugabo Verite <mugaboverite@gmail.com>
2023-01-12 00:01:29 +0200

    chore: applied linting

--- __tests__/isIPv4.spec.ts
+++ __tests__/isIPv4.spec.ts
@@ -2,17 +2,16 @@ import { describe, expect, it } from 'vitest'
 import { isIPv4 } from '../src'
 
 describe('Test IPv4 validator', () => {
-  it('should succeed', () => {
-    expect(isIPv4('192.168.0.3').message).toBeUndefined()
-    expect(isIPv4('192.168.0.3').success).toBeTruthy()
-  })
+	it('should succeed', () => {
+		expect(isIPv4('192.168.0.3').message).toBeUndefined()
+		expect(isIPv4('192.168.0.3').success).toBeTruthy()
+	})
 
-  it('should fail', () => {
-    expect(isIPv4('rrtr3345').success).toBeFalsy()
-  })
+	it('should fail', () => {
+		expect(isIPv4('rrtr3345').success).toBeFalsy()
+	})
 
-
-  it('should fail', () => {
-    expect(isIPv4('::0').message).toBe('::0 is not a valid IPv4')
-  })
+	it('should fail', () => {
+		expect(isIPv4('::0').message).toBe('::0 is not a valid IPv4')
+	})
 })
