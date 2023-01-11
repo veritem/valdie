6a4e9989885687208f9ee9cc709d0b9ce013c4e7
pacifiquem <pacifiquemurangwa001@gmail.com>
2023-01-11 21:03:05 +0200

    fix: isDistinct tests

--- __tests__/isDistinct.spec.ts
+++ __tests__/isDistinct.spec.ts
@@ -1,22 +1,16 @@
-import { describe, expect, it } from "vitest"
+import { describe, expect, it } from 'vitest'
 import { isDistinct } from '../src'
 
-
-
 describe('isDistinct', () => {
-    it('check for distinctArray validity', () => {
-      expect(isDistinct([1, 3, 2, 4, 5, 6, 7, 8, 9, 0]).success).toBeTruthy()
-      expect(isDistinct([[1, 3, 2, 4], [5, 6, 7, 8, 9, 0]]).success).toBeTruthy()
-      expect(isDistinct([true, false]).success).toBeTruthy()
-      expect(isDistinct([1, "string", true]).success).toBeTruthy()
-      expect(isDistinct([{name: "string", age: 21}, {name: "another string", age: 32}]).success).toBeTruthy()
-    });
-  
-    it('check if isDistinct validator fails', () => {
-      expect(isDistinct([1, 1, 3, 4, 2]).success).toBeFalsy()
-      expect(isDistinct([[1, 2, 3], [1, 2, 3]]).success).toBeFalsy()
-      expect(isDistinct([true, true]).success).toBeTruthy()
-      expect(isDistinct([1, "string", 1, "string"]).success).toBeTruthy()
-      expect(isDistinct([{name: "string", age: 21}, {name: "string", age: 21}]).success).toBeTruthy()
-    });
+  it('should succeed .', () => {
+    expect(isDistinct([1, 3, 2, 4, 5, 6, 7, 8, 9, 0]).success).toBeTruthy()
+    expect(isDistinct(["string", "another string"]).success).toBeTruthy()
+    expect(isDistinct([true, false]).success).toBeTruthy()
   })
+
+  it('should fail .', () => {
+    expect(isDistinct([1, 1, 3, 4, 2]).success).toBeFalsy()
+    expect(isDistinct(["string", "string", "another string"]).success).toBeFalsy()
+    expect(isDistinct([true, true]).success).toBeFalsy()
+  })
+})
